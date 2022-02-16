2798bb4cdab15af824cce499ee262becf4c2d67f
Esteban Salguero <Esteban Salguero@192.168.0.15>
2022-02-16 12:04:40 -0600

    Segunda tarea del curso Desarrollo de Apps WEB y Patrones

---
+++ src/main/java/com/hamsters/HamstersApplication.java
@@ -0,0 +1,13 @@
+package com.hamsters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HamstersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HamstersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hamsters/HamstersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hamsters;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HamstersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
